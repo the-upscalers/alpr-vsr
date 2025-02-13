466c54f8516f5c95eef71cf573b8aea500564fdd
Dan Niles <niles_dan@live.com>
2025-02-13 19:49:26 +0530

    Remove react frontend

--- frontend/src/components/VideoPlayer.jsx
+++
@@ -1,7 +0,0 @@
-import { forwardRef } from "react";
-import TwoDVideoAnnotation from "../../../src/app/index";
-
-const VideoPlayer = forwardRef(function VideoPlayer(props, ref) {
-	return <></>;
-});
-export default VideoPlayer;
